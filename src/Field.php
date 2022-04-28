41ba9da51f4da43f6e9ac44161e6b87a45b6d082
shvarev <v.m.shvarev@yandex.ru>
2022-04-28 13:21:37 +0300

    FIX-10 Remove Field::setKey and Field::setValue

--- src/Field.php
+++ src/Field.php
@@ -13,19 +13,9 @@ final class Field
         $this->value = $value;
     }
 
-    public function setKey(mixed $key)
-    {
-        $this->key = $key;
-    }
-
     public function getKey(): mixed
     {
         return $this->key;
-    }
-
-    public function setValue(mixed $value)
-    {
-        $this->value = $value;
     }
 
     public function getValue(): mixed
